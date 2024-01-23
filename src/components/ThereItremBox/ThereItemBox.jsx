227f9d2c4f87b6bd08f48efd1df2e2bc5b844d45
AlirezaSohrabiHT <AlirezaSohrabiHT@gmail.com>
2024-01-23 13:14:20 +0330

    component make and box is in correct size + responsive

--- src/components/ThereItremBox/ThereItemBox.jsx
+++ src/components/ThereItremBox/ThereItemBox.jsx
@@ -5,13 +5,13 @@ const ThereItemBox = (props) => {
     return(
         <>
             <div className={styles.main_container}>
-            <video className={styles.vedio} autoplay="autoplay"id="vid" loop="loop" muted>
+            <video className={styles.vedio1} autoplay="autoplay"id="vid" loop="loop" muted>
             <source src={props.url1} type="video/mp4"/>
             </video>
-            <video className={styles.vedio} autoplay="autoplay"id="vid" loop="loop" muted>
+            <video className={styles.vedio2} autoplay="autoplay"id="vid" loop="loop" muted>
             <source src={props.url2} type="video/mp4"/>
             </video>
-            <video className={styles.vedio} autoplay="autoplay"id="vid" loop="loop" muted>
+            <video className={styles.vedio3} autoplay="autoplay"id="vid" loop="loop" muted>
             <source src={props.url3} type="video/mp4"/>
             </video>
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ThereItemBox from "../components/ThereItremBox/ThereItemBox";
+import FiveItemBox from "../components/FiveItemBox/FiveItemBox"
 import styles from "./Home.module.css"
 const Home = () => {
 
@@ -8,12 +9,14 @@ const Home = () => {
         <div className="container">
             <div className="row">
                 <div className={`col-12 ${styles.main_container}`}>
-                    <div className={`${styles.column} ${styles.right}`}>
-                    <ThereItemBox url1="https://art.joorchin.co/wp-content/uploads/2024/01/header.mp4" 
+                    <div className={`${styles.column_left} ${styles.right}`}>
+                    <FiveItemBox url1="https://art.joorchin.co/wp-content/uploads/2024/01/header.mp4" 
                                 url2="https://art.joorchin.co/wp-content/uploads/2024/01/header.mp4"
-                                url3="https://art.joorchin.co/wp-content/uploads/2024/01/header.mp4" />
+                                url3="https://art.joorchin.co/wp-content/uploads/2024/01/header.mp4"
+                                url4="https://art.joorchin.co/wp-content/uploads/2024/01/header.mp4"
+                                url5="https://art.joorchin.co/wp-content/uploads/2024/01/header.mp4"/>
                     </div>
-                    <div className={`${styles.column}`}>
+                    <div className={`${styles.column_right}`}>
                     <ThereItemBox url1="https://art.joorchin.co/wp-content/uploads/2024/01/header.mp4" 
                                 url2="https://art.joorchin.co/wp-content/uploads/2024/01/header.mp4"
                                 url3="https://art.joorchin.co/wp-content/uploads/2024/01/header.mp4" />
